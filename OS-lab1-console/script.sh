b9be57d9d2cbc10b4e47589194d0a3999852a6bf
Aleksandr Videvich <videvichs@gmail.com>
2022-09-28 16:02:26 +0300

    and again....

--- OS-lab1-console/script.sh
+++ OS-lab1-console/script.sh
@@ -1,7 +1,7 @@
-g++ -c Number/number.cpp -o Number/build/number.o
+g++ -c  Number/number.cpp -o Number/build/number.o
 ar cr Number/build/libNumber.a Number/build/number.o 
 g++ -c Vector/vector.cpp -o Vector/build/vector.o -INumber -LNumber/build -lNumber
-g++ -shared -o Vector/build/libVector.so Vector/build/vector.o
+g++ -shared -fpic -o Vector/build/libVector.so Vector/build/vector.o
 g++ -o testLibs/main testLibs/main.cpp -INumber -LNumber/build -lNumber -IVector  -LVector/build -lVector 
 cd testLibs
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../Vector/build
